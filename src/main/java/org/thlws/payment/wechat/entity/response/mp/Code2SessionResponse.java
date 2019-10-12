22487e393e29e5ae11f3d9136401191a11a336b5
Hanley <hanley@thlws.com>
2019-10-12 18:46:14 +0800

    fix a bog

--- src/main/java/org/thlws/payment/wechat/entity/response/mp/Code2SessionResponse.java
+++ src/main/java/org/thlws/payment/wechat/entity/response/mp/Code2SessionResponse.java
@@ -3,6 +3,8 @@ package org.thlws.payment.wechat.entity.response.mp;
 import com.google.gson.annotations.SerializedName;
 import org.thlws.utils.JsonUtil;
 
+import java.util.Objects;
+
 /**
  * @author HanleyTang 2019/9/13
  *
@@ -70,7 +72,7 @@ public class Code2SessionResponse {
     }
 
     public boolean isSuccess(){
-        return errCode == 0;
+        return (Objects.isNull(errCode)|| errCode == 0);
     }
 
     @Override
